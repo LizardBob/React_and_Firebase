05a26f64f8be84355622053a46b5992e4cef8b5c
LizardBob <krynecki@vp.pl>
2019-03-23 00:22:08 +0100

    base set up project

---
+++ src/components/Account/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Accont = () => (
+  <div>
+    <h1>Accont</h1>
+  </div>
+);
+
+export default Accont;
---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PasswordCahnge = () => (
+  <div>
+    <h1>Pass Change</h1>
+  </div>
+);
+
+export default PasswordCahnge;
